3cb774fc5846c12d34f820559bf363009c92301d
bigskima <our2026project@gmail.com>
2026-08-19 23:57:25 +0100

    Remove obsolete station cylinder scan route

--- apps/lpg-mobile/app/(station)/scan.tsx
+++
@@ -1,3 +0,0 @@
-import { domainQueries } from "../../src/native/api/domains";
-import { ScanWorkspaceScreen } from "../../src/native/ui/ScanWorkspaceScreen";
-export default function Scan() { return <ScanWorkspaceScreen workspace="Station" jobs={domainQueries.stationJobs()} />; }
