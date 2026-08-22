7a75ada0535e8cd292119f739d34e759d2c72104
bigskima <our2026project@gmail.com>
2026-08-22 08:11:32 +0100

    Add customer terms route

---
+++ apps/lpg-mobile/app/policies/customer-terms.tsx
@@ -0,0 +1,5 @@
+import { PolicyDocumentScreen } from "../../src/native/ui/PolicyDocumentScreen";
+
+export default function CustomerTermsRoute() {
+  return <PolicyDocumentScreen policyKey="policy.customer.terms" />;
+}
