99b5a375106fd2aa43345907b66985f7488af0d3
bigskima <our2026project@gmail.com>
2026-09-05 04:00:34 +0100

    Route station operating settings

---
+++ apps/lpg-mobile/app/(station)/operating-settings.tsx
@@ -0,0 +1,2 @@
+import { StationOperatingSettingsScreen } from "../../src/native/ui/StationOperatingSettingsScreen";
+export default function OperatingSettings() { return <StationOperatingSettingsScreen />; }
