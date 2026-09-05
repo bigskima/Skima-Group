f08e8f82b86baeaa8f0004a57145d346bf8de7e5
bigskima <our2026project@gmail.com>
2026-09-05 03:58:08 +0100

    Route customer account settings

---
+++ apps/lpg-mobile/app/(customer)/account-settings.tsx
@@ -0,0 +1,2 @@
+import { WorkspaceAccountSettingsScreen } from "../../src/native/ui/WorkspaceAccountSettingsScreen";
+export default function AccountSettings() { return <WorkspaceAccountSettingsScreen workspace="customer" />; }
