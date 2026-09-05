d46225d8fd8153bdc40ee67d7ab6a489752f0aad
bigskima <our2026project@gmail.com>
2026-09-05 03:58:01 +0100

    Route customer account tools

---
+++ apps/lpg-mobile/app/(customer)/account-tools.tsx
@@ -0,0 +1,2 @@
+import { WorkspaceAccountToolsScreen } from "../../src/native/ui/WorkspaceAccountToolsScreen";
+export default function AccountTools() { return <WorkspaceAccountToolsScreen workspace="customer" />; }
