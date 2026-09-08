e55890959f02ca33770b95bb9eef0ed8f67f3e9f
bigskima <our2026project@gmail.com>
2026-09-08 21:51:35 +0100

    chore: remove superseded local migration version

--- supabase/migrations/20260908207000_partner_application_coverage_trigger_acl.sql
+++
@@ -1,7 +0,0 @@
-begin;
-
--- Trigger-only helper: it must never be callable as a public RPC.
-revoke all on function public.normalize_partner_application_coverage_for_location()
-  from public, anon, authenticated, service_role;
-
-commit;
